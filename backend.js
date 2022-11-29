eb33edddaefec1c285906f1324e125be4b20a065
KovacsIstvanFerenc604 <91594342+KovacsIstvanFerenc604@users.noreply.github.com>
2022-11-29 11:40:20 +0100

    gépház get

--- backend.js
+++ backend.js
@@ -3,8 +3,8 @@ var cors = require('cors')
 const mysql = require('mysql')
 const app = express()
 const port = 3000
-app.use(express.static('kepek'))
-
+app.use(express.static('videokartya_kepek'))
+app.use(express.static('gephaz_kepek'))
 app.use(cors())
 app.use(express.json())
 //------------------------minden videókártya lekérdezés minden adattal
@@ -27,7 +27,25 @@ app.get('/videokartya', (req, res) => {
     
     connection.end()
   })
-
+  app.get('/gephaz', (req, res) => {
+    const mysql = require('mysql')
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'pcosszerako_kif'
+    })
+    
+    connection.connect()
+    
+    connection.query('SELECT * from gephaz', (err, rows, fields) => {
+      if (err) throw err
+    
+      res.send(rows)
+    })
+    
+    connection.end()
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
