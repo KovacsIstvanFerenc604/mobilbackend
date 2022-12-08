4decdeb253d2f342919c7e34078a1f81213ec975
KovacsIstvanFerenc604 <91594342+KovacsIstvanFerenc604@users.noreply.github.com>
2022-12-08 11:40:16 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -48,6 +48,58 @@ app.get('/videokartya', (req, res) => {
     
     connection.end()
   })
+//------------------------minden háttértár lekérdezés minden adattal 
+app.get('/hattertar', (req, res) => {
+  const mysql = require('mysql')
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pcosszerako_kif'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from hattertar', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+//------------------------minden memória lekérdezés minden adattal 
+app.get('/memoria', (req, res) => {
+  const mysql = require('mysql')
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'pcosszerako_kif'
+  })
+  
+  connection.connect()
+  
+  connection.query('SELECT * from memoria', (err, rows, fields) => {
+    if (err) throw err
+  
+    res.send(rows)
+  })
+  
+  connection.end()
+})
+
+//-------------------------videókártya szűrés
+app.post('/szavazat', (req, res) => {
+  kapcsolat()
+  connection.query('insert into szavazat values (null, '+req.body.bevitel1+')', (err, rows, fields) => {
+    if (err) console.log(err)
+  else
+  res.send("Sikerült a felvitel!")
+  })
+  connection.end()
+}) 
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
